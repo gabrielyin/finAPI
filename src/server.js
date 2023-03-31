5587e5f16e0ef6b4f45cb2751a76be21c762e3b7
Gabriel Yin <2021gabrielyin@gmail.com>
2023-03-31 15:55:14 -0300

    criar conta com validação de cpf

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+app.use(express.json())
+
+const customers = [];
 
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body
-    
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    )
+
+    if (customerAlreadyExists) {
+        res.status(400).json({ error: 'customer already exists' })
+    }
+
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
 })
 
 app.listen(3333)
